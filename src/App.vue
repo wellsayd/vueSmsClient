837f10fb1eda6b7510082b655753758db8343378
Brian Wells <bwells@vbgov.com>
2022-04-23 16:50:29 -0400

    progress

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <div class="mx-4">
       <v-alert dismissible type="error" v-model="showErrorInput">
-        Please input valid phone number and message.</v-alert
+        Please check your phone and email inputs.</v-alert
       >
 
       <vue-tel-input-vuetify
@@ -19,6 +19,12 @@
         class="shrink"
       ></v-text-field>
       <v-btn elevation="2" v-on:click="sendMessage">Send Message</v-btn>
+
+      <v-textarea
+        name="input-7-1"
+        v-model="conversationToString"
+        hint="Hint text"
+      ></v-textarea>
     </div>
   </v-app>
 </template>
@@ -35,12 +41,38 @@ export default {
       phoneNumber: "",
       phoneNumberisValid: false,
       showErrorInput: false,
+      conversation: [],
+      connection: null,
     };
   },
+  computed: {
+    conversationToString: {
+      get() {
+        return this.conversation.join("\r");
+      },
+    },
+  },
+
   methods: {
+    parseReceivedMessage(event) {
+      try {
+        var data = JSON.parse(event.data);
+        console.log(data.Records);
+        for (var i = 0; i < data.Records.length; i++) {
+          console.log(data.Records[i].dynamodb.NewImage.message.S);
+
+          this.conversation.push(
+            data.Records[i].dynamodb.NewImage.phone.S +
+              " responded: " +
+              data.Records[i].dynamodb.NewImage.message.S
+          );
+        }
+      } catch (error) {
+        console.log("error = " + error);
+      }
+    },
     phoneInput(formattedNumber, { number, valid }) {
       this.phoneNumber = number.e164;
-      console.log(this.phoneNumber);
       this.phoneNumberisValid = valid;
     },
 
@@ -50,23 +82,42 @@ export default {
         return;
       }
 
-      console.log(this.phoneNumber);
-      console.log(this.message);
       try {
-        var response = await api.post(
+        await api.post(
           "https://vlq8i3k6bf.execute-api.us-east-1.amazonaws.com/dev/sendSms",
           { message: this.message, phone: this.phoneNumber }
         );
+
+        this.conversation.push(this.phoneNumber + " sent: " + this.message);
       } catch (e) {
         console.log("Error = " + e);
+        this.showErrorInput = true;
       }
     },
   },
+
+  /**
+   * Page has mounted
+   */
+  created() {
+    var connection = new WebSocket(
+      "wss://2bqssjlvja.execute-api.us-east-1.amazonaws.com/production"
+    );
+
+    connection.onmessage = function (event) {
+      console.log("event = " + JSON.stringify(event));
+    };
+
+    connection.onopen = function (event) {
+      console.log(event);
+      console.log("Successfully connected to the echo websocket server...");
+    };
+
+    connection.addEventListener(
+      "message",
+      this.parseReceivedMessage.bind(this)
+    );
+  },
 };
 </script>
-<style>
-input.heighttext {
-  padding: 20px 10px;
-  line-height: 28px;
-}
-</style>
+<style></style>
